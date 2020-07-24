e5aa6f059ced11aed6ad2917db6a02cad5d8a414
David <dieavid01@gmail.com>
2020-07-24 10:58:29 +0100

    for mobile

--- src/js/index.js
+++ src/js/index.js
@@ -12,113 +12,162 @@ const GameRules = document.querySelectorAll('.game-rules');
 const playButton = document.querySelector('.splash-screen button:first-of-type');
 const splashScreen = document.querySelector('.splash-screen');
 
-
-
-const test2 = function (min, max) {
-    //  get a random integer between two values
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (min + max));
-}
-
 const duplicatePlayerPick = playerPick.map(pick => pick.cloneNode(true));
 const duplicatePlayerPick2 = playerPick.map(pick => pick.cloneNode(true));
 
-playerPick.forEach((item, i) => {
-    item.addEventListener('click', () => {
-        const cpuPlay = test2(0, 3);
-        switch (item.classList[0]) {
-            case 'paper':
-                signPick.classList.add('hide');
-                battle.classList.add('show');
-                player.prepend(duplicatePlayerPick2[i]);
-                setTimeout(function () {
-                cpu.firstElementChild.classList.remove('dummy');
-                cpu.prepend(duplicatePlayerPick[cpuPlay]);
-                if (cpuPlay == 0) {
-                    result.classList.add('show');
-                    decision.textContent = "you draw";
-                } else if (cpuPlay == 1) {
-                    result.classList.add('show');
-                    decision.textContent = "you lose";
-                } else if (cpuPlay == 2) {
-                    result.classList.add('show');
-                    decision.textContent = "you win";
-                }
-            }, 500);
-                break;
+const testobject = ['paper', 'scissors', 'rock'];
 
-            case 'scissors':
-                signPick.classList.add('hide');
-                battle.classList.add('show');
-                player.prepend(duplicatePlayerPick2[i]);
-                setTimeout(function () {
-                    cpu.firstElementChild.classList.remove('dummy');
-                    cpu.prepend(duplicatePlayerPick[cpuPlay]);
-                    if (cpuPlay == 0) {
-                        result.classList.add('show');
-                        decision.textContent = "you win";
-                    } else if (cpuPlay == 1) {
-                        result.classList.add('show');
-                        decision.textContent = "you draw";
-                    } else if (cpuPlay == 2) {
-                        result.classList.add('show');
-                        decision.textContent = "you lose";
-                    }
-                }, 500);
+const testFunc = function (index) {
+    // signPick.classList.add('hide');
+    signPick.classList.add('slideOutLeft')
+    // signPick.classList.remove('slideInLeft');
+    battle.classList.add('gameSlideIn');
+    battle.classList.remove('slideOutRight');
+    player.prepend(duplicatePlayerPick2[index]);
+}
 
-                break;
-            case 'rock':
-                signPick.classList.add('hide');
-                battle.classList.add('show');
-                player.prepend(duplicatePlayerPick2[i]);
-                setTimeout(function () {
-                cpu.firstElementChild.classList.remove('dummy');
-                cpu.prepend(duplicatePlayerPick[cpuPlay]);
-                if (cpuPlay == 0) {
-                    result.classList.add('show');
-                    decision.textContent = "you lose"
-                } else if (cpuPlay == 1) {
-                    result.classList.add('show');
-                    decision.textContent = "you win"
-                } else if (cpuPlay == 2) {
-                    result.classList.add('show');
-                    decision.textContent = "you draw"
-                }
-            }, 500);
-                break;
+const gameWin = function () {
+    result.classList.add('flipIn');
+    decision.textContent = "you win";
+    player.classList.add('win');
+}
 
+const gameLose = function () {
+    result.classList.add('flipIn');
+    decision.textContent = "you lose";
+    cpu.classList.add('win');
+}
+
+const playerPickPaper = function (cpuPlay) {
+    cpu.firstElementChild.classList.remove('dummy');
+    cpu.prepend(duplicatePlayerPick[cpuPlay]);
+    if (testobject[cpuPlay] === 'scissors') {
+        gameLose();
+    } else if (testobject[cpuPlay] === 'rock') {
+        gameWin();
+    }
+}
+
+const playerPickScissors = function (cpuPlay) {
+    cpu.firstElementChild.classList.remove('dummy');
+    cpu.prepend(duplicatePlayerPick[cpuPlay]);
+    // cpu.firstElementChild.classList.add('growIn');
+    if (cpuPlay == 0) {
+        gameWin();
+    } else if (cpuPlay == 2) {
+        gameLose();
+    }
+}
+
+const playerPickRock = function (cpuPlay) {
+    cpu.firstElementChild.classList.remove('dummy');
+    cpu.prepend(duplicatePlayerPick[cpuPlay]);
+    if (cpuPlay == 0) {
+        gameLose();
+    } else if (cpuPlay == 1) {
+        gameWin();
+    }
+}
+
+const unde = function (item, i) {
+    const cpuPlay = Math.floor(Math.random() * 3);
+    if (item.classList[0] === testobject[cpuPlay]) {
+        setTimeout(function () {
+            result.classList.add('flipIn');
+            decision.textContent = "you draw";
+        }, 1000);
+    }
+    switch (item.classList[0]) {
+        case 'paper':
+            testFunc(i);
+            setTimeout(playerPickPaper, 1200, cpuPlay);
+            break;
+        case 'scissors':
+            testFunc(i);
+            setTimeout(playerPickScissors, 1200, cpuPlay);
+            break;
+        case 'rock':
+            testFunc(i);
+            setTimeout(playerPickRock, 1200, cpuPlay);
+            break;
+        default:
+            break;
+    }
+    scoringSystem();
+}
+
+playerPick.forEach((pick, i) => {
+    pick.addEventListener('click', function () {
+        unde(pick, i);
+    });
+})
+
+const playAgain = function () {
+    signPick.classList.remove('slideOutLeft');
+    signPick.classList.add('slideInLeft');
+    battle.classList.remove('gameSlideIn');
+    battle.classList.add('slideOutRight');
+    setTimeout(function () {
+        player.firstElementChild.remove();
+        cpu.firstElementChild.remove();
+        cpu.firstElementChild.classList.add('dummy');
+        decision.textContent = '';
+        player.classList.remove('win');
+        cpu.classList.remove('win');
+        result.classList.remove('flipIn');
+    }, 300);
+
+}
+
+const closerulesModal = function () {
+    rulesPage.classList.add('rulesFadeOut');
+    rulesPage.classList.remove('rulesFadeIn');
+}
+
+const openRulesModal = function () {
+    rulesPage.classList.add('rulesFadeIn');
+    rulesPage.classList.remove('rulesFadeOut');
+}
+
+const gameStart = function () {
+    game.classList.add('gameSlideIn');
+    const ss = document.querySelectorAll('.sign-wrapper');
+    ss.forEach((s, index) => {
+        s.classList.add('growIn');
+        s.style.animationDelay = `${index/6 + 0.2}s`;
+    })
+
+}
+
+const scoringSystem = function () {
+    let test = document.querySelector('.score');
+    let testValue = parseInt(test.textContent);
+    setTimeout(function () {
+        switch (decision.textContent.toLowerCase()) {
+            case 'you win':
+                testValue += 1;
+                test.textContent = testValue;
+                break;
+            case 'you draw':
+                testValue = testValue;
+                test.textContent = testValue;
+                break;
+            case 'you lose':
+                testValue -= 1;
+                test.textContent = testValue;
+                break;
             default:
                 break;
         }
-    })
-})
+    }, 500)
+}
 
-result.addEventListener('click', () => {
-    battle.classList.remove('show');
-    signPick.classList.remove('hide');
-    player.firstElementChild.remove();
-    cpu.firstElementChild.remove();
-    cpu.firstElementChild.classList.add('dummy');
-    decision.textContent ='';
-})
-
-
-closeRules.addEventListener('click', () => {
-    rulesPage.classList.remove('show');
-    game.classList.add('show');
-})
+result.lastElementChild.addEventListener('click', playAgain);
 
 GameRules.forEach(button => {
-    button.addEventListener('click', () => {
-        rulesPage.classList.add('show');
-        game.classList.remove('show');
-        splashScreen.classList.add('hide');
-    })
+    button.addEventListener('click', openRulesModal);
 })
 
+playButton.addEventListener('click', gameStart);
 
-playButton.addEventListener('click', () => {
-    splashScreen.classList.add('hide');
-    game.classList.add('show');
-})
+closeRules.addEventListener('click', closerulesModal);
