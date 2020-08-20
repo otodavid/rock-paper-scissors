69507cc715f167de050e2626eb3a67f0f8b1a342
David <dieavid01@gmail.com>
2020-08-20 02:21:12 +0100

    finished product

--- src/js/index.js
+++ src/js/index.js
@@ -1,176 +1,151 @@
-const playerPick = Array.from(document.querySelectorAll('.content > div'));
-const signPick = document.querySelector('.sign-pick');
-const battle = document.querySelector('.battle');
-const player = document.querySelector('.player');
-const cpu = document.querySelector('.cpu');
-const result = battle.querySelector('.result');
-const decision = result.querySelector('h1');
-const closeRulesBtn = document.querySelector('.rules i');
-const rulesPage = document.querySelector('.rules');
-const game = document.querySelector('.game');
-const rulesBtn = document.querySelectorAll('.rules-btn');
-const playBtn = document.querySelector('.intro button:first-of-type');
-const intro = document.querySelector('.intro');
+const app = function () {
 
-const duplicatePlayerPick = playerPick.map(pick => pick.cloneNode(true));
-// true so as to copy child nodes as well
-const duplicatePlayerPick2 = playerPick.map(pick => pick.cloneNode(true));
+    let storage = localStorage.getItem('score');
+    const playerPick = Array.from(document.querySelectorAll('.content > div'));
+    const signPick = document.querySelector('.sign-pick');
+    const battle = document.querySelector('.battle');
+    const player = document.querySelector('.player');
+    const cpu = document.querySelector('.cpu');
+    const result = battle.querySelector('.result');
+    const decision = result.querySelector('h1');
+    const closeRulesBtn = document.querySelector('.close-rules-btn');
+    const rulesPage = document.querySelector('.rules');
+    const rulesBtn = document.querySelectorAll('.rules-btn');
+    const playBtn = document.querySelector('.play-btn');
+    const dropShadow = document.querySelector('.drop-shadow');
+    let score = document.querySelector('.score');
+    let scoreValue = parseInt(score.textContent);
+    const newGameBtn = document.querySelector('.new-game-btn');
 
-const allSigns = ['paper', 'scissors', 'rock'];
+    const allSigns = ['paper', 'scissors', 'rock'];
 
-const battleAnimation = function (index) {
-    signPick.classList.remove('slideInLeft');
-    signPick.classList.add('slideOutLeft');
-    battle.className = 'battle SlideInRight';
-    player.prepend(duplicatePlayerPick2[index]);
-}
+    if (storage !== undefined) {
+        score.textContent = storage;
+    }
 
-const gameWin = function () {
-    setTimeout(() => {
-        if (screen.width > 1000) {
-            result.classList.add('sizeIncrease');
+    document.addEventListener('keydown', (e) => {
+        // disable tab key to prevent hidden divs from showing when pressed
+        if (e.keyCode === 9) {
+            e.preventDefault();
         }
+    });
+
+    const startGame = function () {
+        const intro = document.querySelector('.intro');
+        const game = document.querySelector('.game');
+        intro.classList.add('slideOutLeft');
+        game.classList.add('slideInRight', 'present');
+        intro.classList.remove('present');
+        playerPick.forEach((pick, index) => {
+            pick.classList.add('growIn');
+            pick.style.animationDelay = `${index/6 + 0.2}s`;
+        })
+    }
+
+    const openRulesModal = function () {
+        rulesPage.classList.add('slideInTop');
+        rulesPage.classList.remove('slideOutTop');
+        dropShadow.classList.add('active');
+    }
+
+    const closeRulesModal = function () {
+        rulesPage.classList.add('slideOutTop');
+        rulesPage.classList.remove('slideInTop');
+        dropShadow.classList.remove('active');
+    }
+
+    const signSelected = function (item, index) {
+        /* get a random value between 0 and 2 (0 inclusive) and use it as index for any sign picked for CPU; */
+        const cpuPlay = Math.floor(Math.random() * 3);
+        animationAfterplayerPicks(index, cpuPlay);
+
+        setTimeout(() => {
+            switch (item.classList[0]) {
+                case 'paper':
+                    playerPicksPaper(cpuPlay);
+                    break;
+                case 'scissors':
+                    playerPicksScissors(cpuPlay);
+                    break;
+                case 'rock':
+                    playerPicksRock(cpuPlay);
+                    break;
+                default:
+                    break;
+            }
+            scoringSystem();
+
+            localStorage.setItem('score', scoreValue);
+            storage = localStorage.getItem('score');
+        }, 2300);
+    }
+
+    const animationAfterplayerPicks = function (index, cpuPlay) {
+        /* index parameter represents the index number for the sign a player picks while the cpuPlay represents the index number for the sign the CPU picks */
+        signPick.classList.remove('slideInLeft');
+        signPick.classList.add('slideOutLeft');
+        battle.className = 'battle slideInRight';
+        const clone = playerPick[index].cloneNode(true);
+        player.prepend(clone);
+
+        // animation for CPU pick
+        setTimeout(() => {
+            cpu.firstElementChild.classList.remove('dummy');
+            const clone = playerPick[cpuPlay].cloneNode(true);
+            cpu.prepend(clone);
+            if (window.outerWidth > 1000) {
+                battle.className = 'battle compAnimation';
+            }
+        }, 1500);
+    }
+
+    const playerPicksPaper = function (index) {
+        if (allSigns[index] === 'scissors') return gameLose();
+        if (allSigns[index] === 'rock') return gameWin();
+        gameDraw();
+    }
+
+    const playerPicksScissors = function (index) {
+        if (allSigns[index] === 'paper') return gameWin();
+        if (allSigns[index] === 'rock') return gameLose();
+        gameDraw();
+    }
+
+    const playerPicksRock = function (index) {
+        if (allSigns[index] === 'paper') return gameLose();
+        if (allSigns[index] === 'scissors') return gameWin();
+        gameDraw();
+    }
+
+    const gameWin = function () {
+        battleWidthIncrease();
         result.classList.add('flipIn');
         decision.textContent = "you win";
         player.classList.add('winner');
-    }, 1000);
+    }
 
-}
-
-const gameLose = function () {
-    setTimeout(() => {
-        if (screen.width > 1000) {
-            result.classList.add('sizeIncrease');
-        }
+    const gameLose = function () {
+        battleWidthIncrease();
         result.classList.add('flipIn');
         decision.textContent = "you lose";
         cpu.classList.add('winner');
-    }, 1000);
-}
-
-const gameDraw = function () {
-        setTimeout(() => {
-            if (screen.width > 1000) {
-                result.classList.add('sizeIncrease');
-            }
-            result.classList.add('flipIn');
-            decision.textContent = "you draw";
-        }, 1000);
-}
-
-const playerPickPaper = function (index) {
-    cpu.firstElementChild.classList.remove('dummy');
-    cpu.prepend(duplicatePlayerPick[index]);
-    if (screen.width > 1000) {
-        battle.className = 'battle compAnimation';
     }
-    if (allSigns[index] === 'scissors') {
-        gameLose();
-    } else if (allSigns[index] === 'rock') {
-        gameWin();
-    } else {
-        gameDraw();
+
+    const gameDraw = function () {
+        battleWidthIncrease();
+        result.classList.add('flipIn');
+        decision.textContent = "you draw";
     }
-}
 
-const playerPickScissors = function (index) {
-    cpu.firstElementChild.classList.remove('dummy');
-    cpu.prepend(duplicatePlayerPick[index]);
-    if (screen.width > 1000) {
-        battle.className = 'battle compAnimation';
+    const battleWidthIncrease = function () {
+        if (window.outerWidth > 1000) {
+            result.classList.add('sizeIncrease');
+        }
     }
-    if (index == 0) {
-        gameWin();
-    } else if (index == 2) {
-        gameLose();
-    } else {
-        gameDraw();
-    }
-}
 
-const playerPickRock = function (index) {
-    cpu.firstElementChild.classList.remove('dummy');
-    cpu.prepend(duplicatePlayerPick[index]);
-    if (screen.width > 1000) {
-        battle.className = 'battle compAnimation';
-    }
-    if (index == 0) {
-        gameLose();
-    } else if (index == 1) {
-        gameWin();
-    } else {
-        gameDraw();
-    }
-}
+    const scoringSystem = function () {
+        scoreValue = parseInt(score.textContent);
 
-const signSelected = function (item, i) {
-    // get a random value between 0 and 2 and use it as index for any sign picked;
-    const cpuPlay = 0;
-    
-    switch (item.classList[0]) {
-        case 'paper':
-            battleAnimation(i);
-            setTimeout(playerPickPaper, 1200, cpuPlay);
-            break;
-        case 'scissors':
-            battleAnimation(i);
-            setTimeout(playerPickScissors, 1200, cpuPlay);
-            break;
-        case 'rock':
-            battleAnimation(i);
-            setTimeout(playerPickRock, 1200, cpuPlay);
-            break;
-        default:
-            break;
-    }
-    scoringSystem();
-}
-
-playerPick.forEach((pick, i) => {
-    pick.addEventListener('click', function () {
-        signSelected(pick, i);
-    });
-})
-
-const playAgain = function () {
-    signPick.classList.remove('slideOutLeft');
-    signPick.classList.add('slideInLeft');
-    battle.className = 'battle compAnimationRev';
-
-    setTimeout(() => {
-        player.firstElementChild.remove();
-        cpu.firstElementChild.remove();
-        cpu.firstElementChild.classList.add('dummy');
-        decision.textContent = '';
-        player.classList.remove('winner');
-        cpu.classList.remove('winner');
-        result.classList.remove('flipIn', 'sizeIncrease');
-    }, 300);
-}
-
-const closerulesModal = function () {
-    rulesPage.classList.add('slideOutTop');
-    rulesPage.classList.remove('slideInTop');
-}
-
-const openRulesModal = function () {
-    rulesPage.classList.add('slideInTop');
-    rulesPage.classList.remove('slideOutTop');
-}
-
-const gameStart = function () {
-    intro.classList.add('slideOutLeft');
-    game.classList.add('SlideInRight');
-    playerPick.forEach((pick, index) => {
-        pick.classList.add('growIn');
-        pick.style.animationDelay = `${index/6 + 0.2}s`;
-    })
-}
-
-const scoringSystem = function () {
-    let score = document.querySelector('.score');
-    let scoreValue = parseInt(score.textContent);
-    setTimeout(function () {
         switch (decision.textContent.toLowerCase()) {
             case 'you win':
                 scoreValue += 1;
@@ -191,15 +166,52 @@ const scoringSystem = function () {
             default:
                 break;
         }
-    }, 1200)
+    }
+
+    const newGame = function () {
+        localStorage.setItem('score', '0');
+        score.textContent = '0';
+    }
+
+    const playAgain = function () {
+        signPick.classList.remove('slideOutLeft');
+        signPick.classList.add('slideInLeft');
+        battle.className = 'battle compAnimationRev';
+
+        setTimeout(() => {
+            player.firstElementChild.remove();
+            cpu.firstElementChild.remove();
+            cpu.firstElementChild.classList.add('dummy');
+            decision.textContent = '';
+            player.classList.remove('winner');
+            cpu.classList.remove('winner');
+            result.classList.remove('flipIn', 'sizeIncrease');
+        }, 500);
+    }
+
+    function loadEventListeners() {
+        playBtn.addEventListener('click', startGame);
+        rulesBtn.forEach(button => {
+            button.addEventListener('click', openRulesModal);
+        })
+        closeRulesBtn.addEventListener('click', closeRulesModal);
+        dropShadow.addEventListener('click', closeRulesModal);
+        playerPick.forEach((pick, i) => {
+            pick.addEventListener('click', function () {
+                signSelected(pick, i);
+            });
+        });
+        newGameBtn.addEventListener('click', newGame);
+        newGameBtn.addEventListener('click', () => {
+            const bCL = battle.classList;
+            if (bCL.contains('compAnimation') || bCL.contains('slideInRight')) {
+                playAgain();
+            };
+        });
+        result.lastElementChild.addEventListener('click', playAgain);
+    }
+
+    loadEventListeners();
 }
 
-result.lastElementChild.addEventListener('click', playAgain);
-
-rulesBtn.forEach(button => {
-    button.addEventListener('click', openRulesModal);
-})
-
-playBtn.addEventListener('click', gameStart);
-
-closeRulesBtn.addEventListener('click', closerulesModal);
+app();
